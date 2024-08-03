9adb7f27e1504209cd00892822c69597de379073
Anuj Kumar <67889993+anuj8052@users.noreply.github.com>
2024-08-03 16:17:50 +0530

    update

--- mywebsite/mywebsite/views.py
+++ mywebsite/mywebsite/views.py
@@ -5,8 +5,15 @@ from django.shortcuts import render
 def HomePage(request):
     data = {
         'title': 'Home New',
-        'body_title': 'Welcome to django-website!'
+        'body_title': 'Welcome to django-website!',
+        # adding list to use for loop
+        'clist': ['PHP', 'Python', 'Django'],
+        'student_details': [
+            {'name': 'pradeep', 'phone': 8948013408},
+            {'name': 'Anuj', 'phone': 1234567890}
+        ]
     }
+    
     return render(request, "index.html", data)
 
 def aboutUs(request):
